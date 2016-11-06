211479a56aaf9db07ad57c15efc67c390bda7636
ViniciusBezerra94 <vinicius_bezerra@hotmail.com>
2016-11-06 08:06:17 -0200

    config local

--- target/generated-sources/annotations/br/com/stylehair/entity/Cliente_.java
+++ target/generated-sources/annotations/br/com/stylehair/entity/Cliente_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-02T20:02:04")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-02T22:09:01")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ { 
 
